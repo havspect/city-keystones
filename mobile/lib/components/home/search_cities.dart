beb96b984585b02b6fdb6aa0a39be423546a0868
havspec <tim@meyer-hollatz.de>
2025-03-25 02:43:22 +0100

    Added clear field

--- mobile/lib/components/home/search_cities.dart
+++ mobile/lib/components/home/search_cities.dart
@@ -6,9 +6,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final isSelectedProvider = StateProvider<String>((ref) => '');
 
 class SearchCities extends ConsumerWidget {
-  const SearchCities({
-    super.key
-  });
+  const SearchCities({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
@@ -17,7 +15,7 @@ class SearchCities extends ConsumerWidget {
 
     return Autocomplete<City>(
       optionsBuilder: (TextEditingValue textEditingValue) async {
-        final String searchQuery = textEditingValue.text;
+        final searchQuery = textEditingValue.text;
 
         if (isSelected != searchQuery) {
           ref.read(citiesListProvider.notifier).search(searchQuery);
@@ -35,15 +33,34 @@ class SearchCities extends ConsumerWidget {
         ref.read(isSelectedProvider.notifier).state = city.name;
       },
       fieldViewBuilder: (context, controller, focusNode, onFieldSubmitted) {
-        return TextField(
-          controller: controller,
-          focusNode: focusNode,
-          decoration: InputDecoration(
-            labelText: 'Search Cities',
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(10.0),
+        return Stack(
+          children: [
+            TextField(
+              controller: controller,
+              focusNode: focusNode,
+              decoration: InputDecoration(
+                labelText: 'Search Cities',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
             ),
-          ),
+            if (controller.text != '')
+              Positioned.fill(
+                child: Align(
+                  alignment: Alignment.centerRight,
+                  child: IconButton(
+                    icon: Icon(Icons.clear),
+                    onPressed: () {
+                      controller.clear();
+                      ref.refresh(citiesListProvider.future);
+                      ref.read(isSelectedProvider.notifier).state = '';
+                    },
+                    tooltip: 'Clear Search',
+                  ),
+                ),
+              ),
+          ],
         );
       },
     );
